22f5906b29db35dcf9ae9388c42a2dab77a1c97e
Dmitry Zharikov <zdimon77@gmail.com>
2020-03-07 11:04:58 +0200

    quiz

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print('Hello world')
+#!/usr/bin/env python3
+import sys
+
+from mylibs.quiz import quiz
+print('Your name???')
+name = input()
+quiz(name)
+
---
+++ mylibs/quiz.py
@@ -0,0 +1,33 @@
+
+def quiz(user_name):
+    print('Hello %s!!!!!' % user_name)
+    value = 0
+
+    question_list = [
+        {'question': 'What is your name??', 'answers': [
+            {'text': '1. Dima', 'true': True},
+            {'text': '2. Yulia', 'true': False}
+        ]},
+        {'question': 'How old are you?', 'answers': [
+            {'text': '1. 30',  'true': True},
+            {'text': '2. 40', 'true': False}
+        ]}
+    ]
+
+
+    for q in question_list:
+        print(q['question'])
+        for a in q['answers']:
+            print(a['text'])
+        answer = int(input())
+        el = q['answers'][answer-1]
+        if el['true'] == True:
+            print('You are right!!!')
+            value +=  10
+        else:
+            print('You are WRONG!!!')
+
+
+
+    print("You have got %s %s" % (value,value))
+    return True
